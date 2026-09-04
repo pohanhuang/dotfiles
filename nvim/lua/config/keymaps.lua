67daee80132c5c88066bfdd4fbbdb73f499247f0
Po Han Huang <pohan.huang@suse.com>
2026-09-04 17:56:49 +0000

    feat: improve nvim  config
    Signed-off-by: Po Han Huang <pohan.huang@suse.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-o>", "<C-i>", { desc = "Jump forward" })
+vim.keymap.set("n", "<C-i>", "<C-o>", { desc = "Jump backward" })
--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -4,6 +4,18 @@ return {
     keymap = {
       preset = "default",
       ["<M-Esc>"] = { "show", "fallback" },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<CR>'] = { 'accept', 'fallback' },
+    },
+    cmdline = {
+      keymap = {
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+      },
     },
   },
 }
