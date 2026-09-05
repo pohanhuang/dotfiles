6d948dbd471d8859ae6749cb2742f0a8055768f8
Pohan Huang <pohan.huang@suse.com>
2026-09-05 17:25:16 +0800

    nvim: improve keymaps for diffview
    Signed-off-by: pohanhuang <pohan.huang@suse.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,11 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-o>", "<C-i>", { desc = "Jump forward" })
 vim.keymap.set("n", "<C-i>", "<C-o>", { desc = "Jump backward" })
+
+-- Diffview
+require("which-key").add({
+  { "<leader>d", group = "diff" },
+})
+vim.keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
+vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
+vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History" })
