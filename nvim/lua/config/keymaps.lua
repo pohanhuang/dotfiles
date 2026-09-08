9521acff7412fb8f3994c6c954cb82980ab5c1d7
Po Han Huang <pohan.huang@suse.com>
2026-09-08 15:11:16 +0000

    chore: remove diff, have hydra now
    Signed-off-by: Po Han Huang <pohan.huang@suse.com>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,8 +10,3 @@ vim.keymap.set("n", "<C-i>", "<C-o>", { desc = "Jump backward" })
 -- Delete
 vim.keymap.set({ "n", "v" }, "<leader>dd", '"_dd', { desc = "Delete line (no yank)" })
 vim.keymap.set({ "n", "v" }, "<leader>dr", '"_d', { desc = "Delete (no yank)" })
-
--- Diffview
-vim.keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<cr>", { desc = "Diffview Open" })
-vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<cr>", { desc = "Diffview Close" })
-vim.keymap.set("n", "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History" })
