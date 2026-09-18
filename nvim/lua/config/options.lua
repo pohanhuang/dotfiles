3c45580a159022db13dcf15d1103e11205b496f2
Po Han Huang <pohan.huang@suse.com>
2026-09-18 17:04:38 +0000

    feat: options.lua, autocmds.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,23 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- OSC 52 clipboard: write-only (no query) to avoid SSH latency
-vim.g.clipboard = {
-  name = "macOS OSC52 via tmux outer tty",
-
-  copy = {
-    ["+"] = { "/home/po/.config/tmux/scripts/yank-osc52.sh" },
-    ["*"] = { "/home/po/.config/tmux/scripts/yank-osc52.sh" },
-  },
-
-  paste = {
-    ["+"] = false,
-    ["*"] = false,
-  },
-
-  cache_enabled = 0,
-}
-
+-- OSC 52 clipboard, built into nvim: works under tmux, herdr and plain ssh.
+-- (the old yank-osc52.sh wrote to /dev/tty, which clipboard jobs don't have)
+vim.g.clipboard = "osc52"
 vim.opt.clipboard = "unnamedplus"
 vim.g.lazyvim_check_order = false
 
